724056fafcabef1b5df5280795b3dea5333ac362
lou-flan <louise_flanagan@outlook.com>
2020-05-28 11:13:41 +1000

    setup npm dependencies. server running on port. need to define controller methods

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 // requirements
+const express = require('express');
+const cors = require('cors');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const router = express.Router();
+
+const port = process.env.PORT || 3010;
 
 // app.use
+const app = express();
+app.use(bodyParser.json());
 
 // db connection
+//const dbConn = process.env.MONGODB_URI || 'mongodb://localhost:3010/
 
 // set views engine to handlebars
+app.set('view engine', 'hbs')
+app.set('views', './public/templates')
 
-// app.listen
+app.use(express.static('./public'))
 
 // put news routes in here as user can only make a get request on news?
+// app.use('/', newsRouter);
+// router.get('/', showNews);
+
+// app.listen
+app.listen(port, () => {
+    console.log(`News app listening on port ${port} HELL YEAH`);
+});
