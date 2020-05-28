fa4fe342de54019d134c5dc4bd447a1d07752c4c
lou-flan <louise_flanagan@outlook.com>
2020-05-28 11:41:29 +1000

    linked getNews function from utils to controller

--- controllers/news_controller.js
+++ controllers/news_controller.js
@@ -1,5 +1,5 @@
 // methods to call functions from utilities and send the responses back to the client
-const {getNews} = require('./utils/news_utils')
+const {getNews} = require('../utils/news_utils')
 // handles what comes back from DB calls that are defined in the utilities
 
 // res.render(index, {articles})
--- utils/news_utils.js
+++ utils/news_utils.js
@@ -4,5 +4,9 @@
 // methods to access data from news API call and save articles into obj
 
 // methods to call db to check for words then redact and replace words from the news articles before sending to controller to render
-
+const getNews = () => {
+  console.log("this database doesn't exist")  
+}
 // hard code two arrays of safe words and banned words to use before connecting to db
+
+module.exports = {getNews}
