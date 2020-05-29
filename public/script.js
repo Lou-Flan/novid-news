f3bc86a445a1f44c1dfe082ef0b995040d9047d7
lou-flan <louise_flanagan@outlook.com>
2020-05-30 08:55:55 +1000

    created client side function to change words. cleaned up news_utils. added more styling to scss. added text input to html

--- public/script.js
+++ public/script.js
@@ -1,5 +1,22 @@
-// APP 2.0 //
+let text = document.querySelectorAll("p")
+let titles = document.querySelectorAll("h3")
 
-// Can define what to do with text that user enters into browser by defining HTTP requests for word_pairs in here?
+safeWords = ["Malarkey", "Crumpet castle", "The meaning of life", "Donald Duck", "Lolly gag", "Bumfuzzle" ]
 
-// Event listeners etc
+function addWord(event) {
+	event.preventDefault()
+	let wordInput = document.getElementById("word-input")
+    let word = wordInput.value.trim()
+
+    titles.forEach((element) => {
+        elementToChange = element.textContent
+        element.textContent = elementToChange.replace(word, safeWords[Math.floor(Math.random() * safeWords.length)])
+    })
+    
+    text.forEach((element) => {
+        elementToChange = element.textContent
+        element.textContent = elementToChange.replace(word, safeWords[Math.floor(Math.random() * safeWords.length)])
+    })
+}
+
+document.getElementById("submit-word").addEventListener("click", addWord)
--- utils/news_utils.js
+++ utils/news_utils.js
@@ -1,76 +1,66 @@
-const api_key = require('dotenv').config();
+const api_key = require("dotenv").config();
 const api = process.env.API_KEY;
-const fetch = require('node-fetch');
-var moment = require('moment');
-const Words = require('../models/novid_words')
-// API call in here
+const fetch = require("node-fetch");
+var moment = require("moment");
+const Words = require("../models/novid_words");
 
 const getNews = async () => {
-  const url = "https://newsapi.org/v2/top-headlines?country=au&apiKey=" + api;
+  const url = "https://newsapi.org/v2/top-headlines?country=au&pageSize=50&apiKey=" + api;
   try {
     const response = await fetch(url);
     const jsonData = await response.json();
-    // function call here to redact and replace words
-    const cleanedArticles = await articleCleaner(jsonData)
-    return cleanedArticles
-    
-  } catch(error) {
-      console.log(error)
+    const cleanedArticles = await articleCleaner(jsonData);
+    return cleanedArticles;
+  } catch (error) {
+    console.log(error);
   }
-}
-
-//GET on database async getWords()
-// return await {safe, prohibited}
+};
 
 const getWords = async () => {
-  let result = await Words.find()
-  let safeWords = []
-  let unSafeWords = []
+  let result = await Words.find();
+  let safeWords = [];
+  let unSafeWords = [];
 
-  result.forEach((word) =>{
-    word.type === "safe" ? safeWords.push(word.word) : unSafeWords.push(word.word)
-  }
-  )
+  result.forEach((word) => {
+    word.type === "safe"
+      ? safeWords.push(word.word)
+      : unSafeWords.push(word.word);
+  });
 
-   return {safeWords, unSafeWords}
-}
+  return { safeWords, unSafeWords };
+};
 
 const articleCleaner = async (data) => {
   let articles = data.articles;
 
-  let dbWords = await getWords()
-  console.log("from the cleaner",dbWords)
-  const safeWords = dbWords.safeWords
-  const unSafeWords = dbWords.unSafeWords
-  
+  let dbWords = await getWords();
+  console.log("from the cleaner", dbWords);
+  const safeWords = dbWords.safeWords;
+  const unSafeWords = dbWords.unSafeWords;
 
-  let cleanArticles = []
+  let cleanArticles = [];
 
   articles.forEach((article) => {
-
-    
-    let regex = new RegExp("\\b"+unSafeWords.join('|')+"\\b","gi")
+    let regex = new RegExp("\\b" + unSafeWords.join("|") + "\\b", "gi");
     article.redacted = false;
+
     if (!article.title || !article.description) {
-      console.log("missing article title or description")
-    } else if(article.title.match(regex) || article.description.match(regex)){
-        article.redacted = true;
-      }
-    article.title = article.title.replace(regex, safeWords[Math.floor(Math.random() * safeWords.length)])
-    article.description = article.description.replace(regex, safeWords[Math.floor(Math.random() * safeWords.length)])
-    article.publishedAt = moment(article.publishedAt).format("MMMM Do, h:mm a"); 
-    cleanArticles.push(article)
-    // console.log(cleanArticles)
-  })
-  return cleanArticles
-}
+      console.log("missing article title or description");
+    } else if (article.title.match(regex) || article.description.match(regex)) {
+      article.redacted = true;
+    }
+    article.title = article.title.replace(
+      regex,
+      safeWords[Math.floor(Math.random() * safeWords.length)]
+    );
+    article.description = article.description.replace(
+      regex,
+      safeWords[Math.floor(Math.random() * safeWords.length)]
+    );
+    article.publishedAt = moment(article.publishedAt).format("MMMM Do, h:mm a");
+    cleanArticles.push(article);
+  });
+  return cleanArticles;
+};
 
-
-
-// methods to access data from news API call and save articles into obj
-
-// methods to call db to check for words then redact and replace words from the news articles before sending to controller to render
-
-// hard code two arrays of safe words and banned words to use before connecting to db
-
-module.exports = {getNews, getWords}
+module.exports = { getNews, getWords };
